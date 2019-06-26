873a267ce12c3d9ba332c2dc1f7fe88214d351a1
Colin Hunwald <colin.hunwald@gmail.com>
2019-06-26 19:46:43 +0200

    Add snapshot

--- src/app/register/register-detail/register-detail.component.ts
+++ src/app/register/register-detail/register-detail.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Record } from "../../_interface/record.model";
+import { Router, ActivatedRoute } from "@angular/router";
+import { RegisterService } from "../register.service";
 @Component({
   selector: 'app-register-detail',
   templateUrl: './register-detail.component.html',
   styleUrls: ['./register-detail.component.css']
 })
 export class RegisterDetailComponent implements OnInit {
+  public records: Array<Record> = [];
+  public record: Array<Record> = [];
+  public showProducts;
 
-  constructor() { }
+  constructor(
+    private register: RegisterService,
+    private router: Router,
+    private activeRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.register.getRecords()
+    .subscribe(data => this.records = data);
+  }
+  getRegisterDetail() {
+    const ide: string = this.activeRoute.snapshot.params.id;
+    this.record = this.records.filter(rec => rec.id === ide);
+    console.log(this.record[0].userDetail.email);
+
   }
 
 }
