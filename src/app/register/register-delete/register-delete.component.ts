255e800461b5ff8075b08bcf3bc2afefbfd8b175
Colin Hunwald <colin.hunwald@gmail.com>
2019-07-19 11:14:13 +0200

    register-delete

--- src/app/register/register-delete/register-delete.component.ts
+++ src/app/register/register-delete/register-delete.component.ts
@@ -4,7 +4,6 @@ import { RegisterService } from '../register.service';
 import { Observable } from 'rxjs';
 import { RecordCreation } from 'src/app/_interface/record-creation';
 import { Location } from '@angular/common';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-delete',
@@ -13,7 +12,6 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterDeleteComponent implements OnInit {
   public record: Observable<RecordCreation[]>;
-  public recordForm: FormGroup;
 
   constructor(
     private activeRoute: ActivatedRoute,
@@ -26,13 +24,6 @@ export class RegisterDeleteComponent implements OnInit {
 
     this.register.getToUpdate(ide);
     this.record = this.register.recUpdate$;
-
-    this.recordForm = new FormGroup({
-      id: new FormControl(''),
-      user: new FormControl(''),
-      action: new FormControl(''),
-      dateOfAction: new FormControl('')
-    });
 
   }
 
