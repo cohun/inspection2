6261f674b63e601e6a39abb14c239e55fb32434e
Colin Hunwald <colin.hunwald@gmail.com>
2019-09-20 19:09:34 +0200

    responsive productGroup

--- src/app/user/la/textile/textile.component.ts
+++ src/app/user/la/textile/textile.component.ts
@@ -7,10 +7,22 @@ import { Location } from '@angular/common';
   styleUrls: ['./textile.component.css']
 })
 export class TextileComponent implements OnInit {
+  public w: number;
 
   constructor(private location: Location) { }
 
   ngOnInit() {
+    const width = window.innerWidth;
+    if (width <= 411) {
+      console.log('phone detected');
+      this.w = 1;
+    } else if (width <= 768) {
+      console.log('mobile device detected');
+      this.w = 2;
+    } else {
+      console.log('desktop detected');
+      this.w = 4;
+    }
   }
   onCancel() {
     this.location.back();
--- src/app/user/product-group/product-group.component.ts
+++ src/app/user/product-group/product-group.component.ts
@@ -7,10 +7,21 @@ import { Location } from '@angular/common';
   styleUrls: ['./product-group.component.css']
 })
 export class ProductGroupComponent implements OnInit {
-
+  public w: number;
   constructor(private location: Location) { }
 
   ngOnInit() {
+    const width = window.innerWidth;
+    if (width <= 411) {
+      console.log('phone detected');
+      this.w = 1;
+    } else if (width <= 768) {
+      console.log('mobile device detected');
+      this.w = 2;
+    } else {
+      console.log('desktop detected');
+      this.w = 4;
+    }
   }
   onCancel() {
     this.location.back();
